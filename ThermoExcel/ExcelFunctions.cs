8b72d57c0acbca673866022e474dbd6abdba84c7
WSifl <WSifl@DESKTOP-GSLP2M4>
2017-11-20 15:52:24 -0500

    Add project files.

---
+++ ThermoExcel/ExcelFunctions.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+using Thermodynamics;
+
+using Therm = Thermodynamics.Thermo;
+using ML = Monolith;
+
+namespace ThermoExcel
+{
+    public static class MyFunctions
+    {
+        public static ExcelInt myExcelInt = new ExcelInt();// seem to need this so that interface functions are not exported
+
+        [ExcelFunction(Description = "Lists the type of units that can be converted to units of the same or similar types")]
+        public static object[,] UnitTypesList_([ExcelArgument("Enter true for a column list")]bool isCol = false)
+        {
+
+            List<string> ListOfTypes = ML.CallHandler.ListUnitTypes();
+            string[] myTypes = ListOfTypes.ToArray();
+
+            object[,] ret;
+            if (isCol)
+                ret = myExcelInt.oneDToColArray(myTypes);
+            else
+                ret = myExcelInt.oneDToRowArray(myTypes);
+
+            return ret;
+        }
+
+        [ExcelFunction(Description = "Converts a number from an input unit to that of an output unit of the same type")]
+        public static object UnitConvert_([ExcelArgument("Value to be converted")]     double valueIn,
+                                            [ExcelArgument("Original units")]            string inUnit,
+                                            [ExcelArgument("Desired units")]            string outUnit,
+                                            [ExcelArgument("Mol Wt, Density or valid gas or feedstock name")] object mwOrGasName)
+        {
+            return ML.CallHandler.ConvertUnits(inUnit, outUnit, valueIn, mwOrGasName);
+        }
+
+        [ExcelFunction(Description = "Lists the valid units for a specific type of unit")]
+        public static object[,] UnitsList_([ExcelArgument("Enter a valid unit type")]string unitType, [ExcelArgument("Enter true for a column list")]bool isCol = false)
+        {
+            string[] myString = ML.CallHandler.ListUnits(unitType);
+
+            object[,] ret;
+            if (isCol)
+                ret = myExcelInt.oneDToColArray(myString);
+            else
+                ret = myExcelInt.oneDToRowArray(myString);
+
+            return ret;
+        }
+
+        [ExcelFunction(Description = "Lists the species in Thermo Database")]
+        public static object[,] SpeciesList_([ExcelArgument("Enter true for a row list")]bool isCol = true)
+        {
+            string[] spNames;
+            try
+            { spNames = Therm.ListSpecies(); }
+            catch (Exception e)
+            { return myExcelInt.returnExceptionMessage("Exception in ListSpecies  --  ", e); }
+
+            object[,] ret;
+            if (isCol)
+                ret = myExcelInt.oneDToColArray(spNames);
+            else
+                ret = myExcelInt.oneDToRowArray(spNames);
+
+            return ret;
+        }
+
+        [ExcelFunction(Description = "returns log10 of the equilibrium constant of formation")]
+        public static object logKf_([ExcelArgument("temperature, C")]double t, 
+                                [ExcelArgument("specie name")]string spName)
+        {
+            object otdr = ML.CallHandler.ThermoJANAF(spName, t);
+            if (otdr.GetType() != typeof(Thermo.ThermoDataRecord))
+            {
+                object[] retS = new object[1];
+                retS[0] = otdr;
+                return myExcelInt.retRowArray(retS);
+            }
+
+            Thermo.ThermoDataRecord tdr = (Thermo.ThermoDataRecord)otdr;
+            return tdr.log10Kf;
+        }
+
+        [ExcelFunction(Description = "returns the enthalpy")]
+        public static object Enthalpy_([ExcelArgument("temperature, C")]double tC,
+                        [ExcelArgument("specie name")]string spName)
+        {
+          return ML.CallHandler.Enthalpy(tC, spName);
+        }
+
+
+        [ExcelFunction(Description = "Calculates chemical equilibrium for a given temperature and pressure, kmol/kg")]
+        public static object EquilibriumTP_(  [ExcelArgument("temperature, C")]double t, 
+                                            [ExcelArgument("pressure, bar gage")]double p,
+                                            [ExcelArgument("atoms H")]double aH,
+                                            [ExcelArgument("atoms C")]double aC,
+                                            [ExcelArgument("atoms N")]double aN,
+                                            [ExcelArgument("atoms O")]double aO,
+                                            [ExcelArgument("atoms Ar")]double aAr,
+                                            [ExcelArgument("atoms s")]double aS)
+        {
+            double[] myAtoms = new double[6];
+            myAtoms[0] = aH;
+            myAtoms[1] = aC;
+            myAtoms[2] = aN;
+            myAtoms[3] = aO;
+            myAtoms[4] = aAr;
+            myAtoms[5] = aS;
+            object[] myOut;
+            myOut = ML.CallHandler.EquilibriumTP_(t, p, myAtoms);
+            return myExcelInt.retColArray(myOut);
+        }
+
+        [ExcelFunction(Description = "Thermo data consistent with JANAF tables")]
+        public static object[,] Thermo_JANAF([ExcelArgument("temperature, C")] double t, [ExcelArgument("specie name")] string spName )
+        {
+            object otdr = ML.CallHandler.ThermoJANAF(spName, t);
+            if (otdr.GetType() != typeof(Thermo.ThermoDataRecord))
+            {
+                object[] retS = new object[1];
+                retS[0] = otdr;
+                return myExcelInt.retRowArray(retS);
+            }
+
+            Thermo.ThermoDataRecord tdr = (Thermo.ThermoDataRecord)otdr;
+            object[] ret = new object[7];
+            ret[0] = tdr.cP;
+            ret[1] = tdr.s;
+            ret[2] = tdr.gHTRef_T;
+            ret[3] = tdr.hHTRef / 1000;
+            ret[4] = tdr.deltaHf / 1000;
+            ret[5] = tdr.deltaGf / 1000;
+            ret[6] = tdr.log10Kf;
+
+            return myExcelInt.retRowArray(ret);
+        }
+
+        [ExcelFunction(Description = "Thermo data consistent with CEA Thermo Build output")]
+        public static object[,] Thermo_CEA([ExcelArgument("temperature, C")] double t, [ExcelArgument("specie name")] string spName)
+        {
+            object otdr = ML.CallHandler.ThermoJANAF(spName, t);
+            if (otdr.GetType() != typeof(Thermo.ThermoDataRecord))
+            {
+                object[] retS = new object[1];
+                retS[0] = otdr;
+                return myExcelInt.retRowArray(retS);
+            }
+
+            Thermo.ThermoDataRecord tdr = (Thermo.ThermoDataRecord)otdr;
+            object[] ret = new object[7];
+            int spIndex = Thermo.findSpecieIndex(spName);
+            ret[0] = tdr.cP;
+            ret[1] = tdr.hHTRef / 1000;
+            ret[2] = tdr.s;
+            ret[3] = tdr.gHTRef_T;
+            ret[4] = Thermo.allSpecies[spIndex].heatForm/1000 + tdr.hHTRef / 1000;
+            ret[5] = tdr.deltaHf / 1000;
+            ret[6] = tdr.log10Kf;
+
+            return myExcelInt.retRowArray(ret);
+        }
+
+        public static object MolWt_(string name)
+        {
+            string[] spName = new string[] { name };
+            return ML.CallHandler.MolWt(spName);
+        }
+    }
+}
---
+++ ThermoExcel/InterfaceFunctions.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+//using ExcelDna.Utilities;
+using System.Diagnostics;
+
+namespace ThermoExcel
+{
+
+    public class ExcelInt
+    {
+        // ************************************************************************************************
+        // All inputs from Excel are 2D
+        // I am making all outputs to Excel 2D so that vectors can be outputted as either columns or rows
+        // *************************************************************************************************
+
+        /* public class MyFunctions
+         {
+             // This function returns a string that describes its argument.
+             // For arguments defined as object type, this shows all the possible types that may be received.
+             // Also try this function after changing the 
+             // [ExcelArgument(AllowReference=true)] attribute.
+             // In that case we allow references to be passed (registerd as type R). 
+             // By default the function will be registered not
+             // to receive references AllowReference=false (type P).
+             [ExcelFunction(Description = "Describes the value passed to the function.", IsMacroType = true)]
+             public static string Describe([ExcelArgument(AllowReference = false)]object arg)
+             {
+                 if (arg is double)
+                     return "Double: " + (double)arg;
+                 else if (arg is string)
+                     return "String: " + (string)arg;
+                 else if (arg is bool)
+                     return "Boolean: " + (bool)arg;
+                 else if (arg is ExcelError)
+                     return "ExcelError: " + arg.ToString();
+                 else if (arg is object[,])
+                     // The object array returned here may contain a mixture of different types,
+                     // reflecting the different cell contents.
+                     return string.Format("Array[{0},{1}]", ((object[,])arg).GetLength(0), ((object[,])arg).GetLength(1));
+                 else if (arg is ExcelMissing)
+                     return "Missing";
+                 else if (arg is ExcelEmpty)
+                     return "Empty";
+                 else if (arg is ExcelReference)
+                     return "Reference: " + XlCall.Excel(XlCall.xlfReftext, arg, true);
+                 else
+                     return "!?Unheard Of";
+             }*/
+
+        internal static bool IsEmpty(object arg)
+        {
+            if (arg is ExcelEmpty)
+                return true;
+            else
+                return false;
+        }
+
+        internal static bool IsMissing(object arg)
+        {
+            if (arg is ExcelMissing)
+                return true;
+            else
+                return false;
+        }
+
+        [ExcelFunction(IsMacroType = true)]
+        internal static string GetWorkBookName()
+        {
+            object reference = (ExcelReference)XlCall.Excel(XlCall.xlfCaller);
+            string sheetName = (string)XlCall.Excel(XlCall.xlSheetNm, reference);
+            string nameLessBrak = sheetName.TrimStart((char)91);
+            int locPeriod = nameLessBrak.IndexOf(".", 0);
+            string name;
+            if (locPeriod < 0)
+                name = nameLessBrak;
+            else
+                name = nameLessBrak.Remove(locPeriod);
+
+            return name;
+        }
+
+        public class MyArray
+        {
+            public int nRows;
+            public int nCols;
+            public int n1D;
+            public bool is1DArray = false;
+            public bool is2DArray = false;
+            public bool isRowArray = false;
+            public bool isColArray = false;
+            public object[,] array;
+            public MyArray(object[,] myA)
+            {
+                array = myA;
+                nRows = array.GetLength(0);
+                nCols = array.GetLength(1);
+                if (nRows > 1 && nCols > 1)
+                    is2DArray = true;
+
+                is1DArray = true;
+
+                if (nCols > 1)
+                {
+                    n1D = nCols;
+                    isRowArray = true;
+                }
+                else
+                {
+                    n1D = nRows;
+                    isColArray = true;// default for 1 cell array
+                }
+            }
+        }
+
+        //public object[] oneDToCSArray
+        //{// returns a C# one d array
+        //    object[] ret = new object[]
+
+        //}
+        public object[,] oneDToColArray(object[] ar)
+        {// use for One D inputs
+            object[,] ret = new object[ar.Length, 1];
+            for (int i = 0; i < ar.Length; i++)
+                ret[i, 0] = ar[i];
+            return ret;
+        }
+
+        public object[,] oneDToRowArray(object[] ar)
+        {// use for One D inputs
+            object[,] ret = new object[1, ar.Length];
+            for (int i = 0; i < ar.Length; i++)
+                ret[0, i] = ar[i];
+            return ret;
+        }
+
+        public object[,] oneDToColArray(int Length)
+        {// use with inputs
+            object[,] ret = new object[Length, 1];
+            return ret;
+        }
+
+        public object[,] oneDToRowArray(int Length)
+        {// use with inputs
+            object[,] ret = new object[Length, 1];
+            return ret;
+        }
+
+        public object[,] retColArray(object[] ar)
+        {
+            object[,] ret = new object[ar.Length, 1];
+            for (int i = 0; i < ar.Length; i++)
+                ret[i, 0] = ar[i];
+            return ret;
+        }
+
+        public object[,] retRowArray(object[] ar)
+        {
+            object[,] ret = new object[1, ar.Length];
+            for (int i = 0; i < ar.Length; i++)
+                ret[0, i] = ar[i];
+            return ret;
+        }
+
+        public object[,] returnMessage(string message)
+        {
+            object[,] ret = new object[1, 1];
+            ret[0, 0] = message;
+            return ret;
+        }
+
+        public object[,] returnExceptionMessage(string message, Exception e)
+        {
+            object[,] ret = new object[1, 1];
+            ret[0, 0] = message + e.Message;
+            return ret;
+        }
+
+    }// end ExcelInt
+
+}// end namespace CBExcel
